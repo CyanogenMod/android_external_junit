9b748c108add13f83ea264150b7f741f05a139c7
Brett Chabot <brettchabot@google.com>
2013-09-26 18:35:01 -0700

    Hide org.junit.internal from droiddoc.
    Bug 10749552
    
    Change-Id: I35efd89db623793d0757353106770d1988af169b

--- src/org/junit/internal/requests/package-info.java
+++ src/org/junit/internal/requests/package-info.java
@@ -2,5 +2,6 @@
  * Provides implementations of {@link org.junit.runner.Request}.
  *
  * @since 4.0
+ * @hide
  */
 package org.junit.internal.requests;
--- src/org/junit/internal/runners/package-info.java
+++ src/org/junit/internal/runners/package-info.java
@@ -2,5 +2,6 @@
  * Provides implementations of {@link org.junit.runner.Runner}
  *
  * @since 4.0
+ * @hide
  */
 package org.junit.internal.runners;
